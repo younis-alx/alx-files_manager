ecc19ae03f2bd03af791d2fe6c8db5d29292fdd8
jay-alx <126336909+younis-alx@users.noreply.github.com>
2024-03-10 07:12:57 +0000

    update db.js

--- utils/db.js
+++ utils/db.js
@@ -6,19 +6,15 @@ const { pwdHashed } = require('./utils');
 
 class DBClient {
   constructor() {
-    const host = process.env.DB_HOST || 'localhost';
-    const port = process.env.DB_PORT || 27017;
-    this.database = process.env.DB_DATABASE || 'files_manager';
+    const host = (process.env.DB_HOST) ? process.env.DB_HOST : 'localhost';
+    const port = (process.env.DB_PORT) ? process.env.DB_PORT : 27017;
+    this.database = (process.env.DB_DATABASE) ? process.env.DB_DATABASE : 'files_manager';
     const dbUrl = `mongodb://${host}:${port}`;
     this.connected = false;
     this.client = new MongoClient(dbUrl, { useUnifiedTopology: true });
-    this.client.connect()
-      .then(() => {
-        this.connected = true;
-      })
-      .catch((err) => {
-        console.error('Failed to connect to the database:', err);
-      });
+    this.client.connect().then(() => {
+      this.connected = true;
+    }).catch((err) => console.log(err.message));
   }
 
   isAlive() {
@@ -26,70 +22,49 @@ class DBClient {
   }
 
   async nbUsers() {
-    try {
-      await this.client.connect();
-      const users = await this.client.db(this.database).collection('users').countDocuments();
-      return users;
-    } catch (err) {
-      console.error('Error retrieving the number of users:', err);
-      throw err;
-    }
+    await this.client.connect();
+    const users = await this.client.db(this.database).collection('users').countDocuments();
+    return users;
   }
 
   async nbFiles() {
-    try {
-      await this.client.connect();
-      const files = await this.client.db(this.database).collection('files').countDocuments();
-      return files;
-    } catch (err) {
-      console.error('Error retrieving the number of files:', err);
-      throw err;
-    }
+    await this.client.connect();
+    const users = await this.client.db(this.database).collection('files').countDocuments();
+    return users;
   }
 
   async createUser(email, password) {
-    try {
-      const hashedPwd = pwdHashed(password);
-      await this.client.connect();
-      const user = await this.client.db(this.database).collection('users').insertOne({ email, password: hashedPwd });
-      return user;
-    } catch (err) {
-      console.error('Error creating a user:', err);
-      throw err;
-    }
+    const hashedPwd = pwdHashed(password);
+    await this.client.connect();
+    const user = await this.client.db(this.database).collection('users').insertOne({ email, password: hashedPwd });
+    return user;
   }
 
   async getUser(email) {
-    try {
-      await this.client.connect();
-      const user = await this.client.db(this.database).collection('users').findOne({ email });
-      return user;
-    } catch (err) {
-      console.error('Error retrieving a user:', err);
-      throw err;
+    await this.client.connect();
+    const user = await this.client.db(this.database).collection('users').find({ email }).toArray();
+    if (!user.length) {
+      return null;
     }
+    return user[0];
   }
 
   async getUserById(id) {
-    try {
-      const _id = new mongo.ObjectID(id);
-      await this.client.connect();
-      const user = await this.client.db(this.database).collection('users').findOne({ _id });
-      return user;
-    } catch (err) {
-      console.error('Error retrieving a user by ID:', err);
-      throw err;
+    const _id = new mongo.ObjectID(id);
+    await this.client.connect();
+    const user = await this.client.db(this.database).collection('users').find({ _id }).toArray();
+    if (!user.length) {
+      return null;
     }
+    return user[0];
   }
 
-  async userExists(email) {
-    try {
-      const user = await this.getUser(email);
-      return !!user;
-    } catch (err) {
-      console.error('Error checking if a user exists:', err);
-      throw err;
+  async userExist(email) {
+    const user = await this.getUser(email);
+    if (user) {
+      return true;
     }
+    return false;
   }
 }
 
